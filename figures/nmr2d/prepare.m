f4c729432eae4096ddc759d54d1aec54e46f0598
Bradley Worley <geekysuavo@gmail.com>
2017-08-23 14:31:13 +0200

    Bit of preparation for 2D NMR

--- figures/nmr2d/prepare.m
+++ figures/nmr2d/prepare.m
@@ -1,18 +1,25 @@
 
 % set the input directory.
 D = '../../examples/nmr2d/';
+addpath(D);
 
 % load the instance data.
 load([D, 'instance.dat.gz']);
 
 % load the models.
-vbcs = load([D, 'model-vbcs.dat.gz']);
-vrvm = load([D, 'model-vrvm.dat.gz']);
+%vbcs = load([D, 'model-vbcs.dat.gz']);
+%nesta = load([D, 'model-nesta.dat.gz']);
+
+% build index vectors.
+ii = vec(repmat([1 : n(1)]', n(2), 1));
+jj = vec(repmat([1 : n(2)], n(1), 1));
+
+% compute the ground truth spectrum.
+x0 = sumsq(mxifft2(y0, n), 3);
+x0 = vec(fftshift(x0));
 
 % collect the necessary data.
-ax = linspace(-pi, pi, n)';
-dat = [vrvm.x, vbcs.x];
-dat = [ax, real(fftshift(dat, 1))];
+dat = [ii, jj, x0];
 
 % write the data to a text file.
 save('-ascii', 'nmr2d.dat', 'dat');
--- figures/nmr2d/render.gp
+++ figures/nmr2d/render.gp
@@ -13,21 +13,17 @@ set rmargin 0.2
 set multiplot layout 1, 3
 
 unset key
-set xlabel '$\omega$' off 0, 0.5
-set xtics ('{\ft $-\pi$}' -pi, '{\ft %g}' 0, '{\ft $\pi$}' pi)
-set ytics ('{\ft %g}' 0)
+set xlabel '$\omega_1$' off 0, 0.5
+set ylabel '$\omega_2$'
+unset xtics
 unset ytics
-set xrange [-pi : pi]
 
-set yrange [-0.02 : 0.7]
 set label 1 '$\m{\Phi}^* \m{y}^0$' at graph lx, ly
 p 'nmr2d.dat' u 1:2 w l lc rgb 'black'
 
-set yrange [-0.05 : 2.8]
 set label 1 '$\xhat_{\textsc{nesta}}$' at graph lx, ly
 p 'nmr2d.dat' u 1:3 w l lc rgb 'black'
 
-set yrange [-0.1 : 4.4]
 set label 1 '$\xhat_{\textsc{vbcs}}$' at graph lx, ly
 p 'nmr2d.dat' u 1:4 w l lc rgb 'black'
 
